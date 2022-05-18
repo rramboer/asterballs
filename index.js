2c9daade53198970073ff5620a751cc57db8dcf7
Ryan Ramboer <ryanramboer@gmail.com>
2022-05-18 02:01:05 -0400

    add player

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-console.log(canvas);
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+}
+
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+}
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const player = new Player(x, y, 30, 'blue')
+player.draw()
+
+addEventListener('click', (event) => {
+    const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null)
+    projectile.draw()
+})
