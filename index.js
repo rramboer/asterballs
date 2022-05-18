391b2bb38c6bb5d883ebb99b417157b8270c4ba5
Ryan Ramboer <ryanramboer@gmail.com>
2022-05-18 02:54:32 -0400

    introduce randomness

--- index.js
+++ index.js
@@ -75,14 +75,25 @@ const enemies = []
 
 function spawnEnemies() {
     setInterval(() => {
-        const x = 100
-        const y = 100
-        const radius = 30
-        const color = 'green'
-        const velocity = {
-            x: 1, 
-            y: 1
+        const radius = Math.random() * (30 - 4) + 4
+        let x
+        let y
+
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random < 0.5 ? 0 - radius : canvas.height + radius
         }
+
+        const color = 'green'
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+
         enemies.push(new Enemy(x, y, radius, color, velocity))
     }, 1000)
 }
@@ -95,13 +106,13 @@ function animate() {
         projectile.update()
     })
 
-    enemies.forEach(enemy => {
+    enemies.forEach((enemy) => {
         enemy.update()
     })
 }
 
 addEventListener('click', (event) => {
-    const angle = Math.atan2(event.clientY - y, event.clientX - x)
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
     const velocity = {
         x: Math.cos(angle),
         y: Math.sin(angle)
@@ -110,3 +121,4 @@ addEventListener('click', (event) => {
 })
 
 animate()
+spawnEnemies()
